3749f5748d912c8b972114257b95f7820588a242
NielsGregers <niels@jumpto365.com>
2020-05-31 10:46:11 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a4dabdcdd2e704e172d9ad94235491f.js"
+  "/precache-manifest.4ff288cbea4706734fcab99347d19804.js"
 );
 
 self.addEventListener('message', (event) => {
